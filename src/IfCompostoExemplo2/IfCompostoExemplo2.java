32ca91f29b3a99490b0d277fa4dc4bdbf76dcdb5
maxdevsr <macson_sr@hotmail.com>
2021-11-16 09:41:00 -0300

    treinamento

--- src/IfCompostoExemplo2/IfCompostoExemplo2.java
+++ src/IfCompostoExemplo2/IfCompostoExemplo2.java
@@ -1,16 +1,24 @@
 package IfCompostoExemplo2;
 
+import java.util.Scanner;
+
 public class IfCompostoExemplo2 {
 
 	public static void main(String[] args) {
-		int media = 6;
+		Scanner sc = new Scanner(System.in);
+		int media;
+		System.out.println("Digite sua nota");
+		media = sc.nextInt();
+
 		String status;
-		if (media >=6) {
+		if (media >= 6) {
 			status = "Aprovado";
 		} else {
 			status = "Reprovado";
 		}
 		System.out.println(status);
+
+		sc.close();
 	}
 
 }
---
+++ src/iftreino/treinoif.java
@@ -0,0 +1,17 @@
+package iftreino;
+
+public class treinoif {
+
+	public static void main(String[] args) {
+		int x = 5;
+
+		System.out.println("Bom dia");
+
+		if (x < 0) {
+			System.out.println("Boa tarde");
+		}
+
+		System.out.println("Boa noite");
+	}
+
+}
